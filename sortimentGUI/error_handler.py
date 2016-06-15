aabab89dddd4f301f92456c2049d9c0093ed10e1
Peter Vašut <peto1994@gmail.com>
2016-06-15 14:46:02 +0200

    Updated error handling documentation

--- sortimentGUI/error_handler.py
+++ sortimentGUI/error_handler.py
@@ -8,6 +8,15 @@ import window_creator
 
 
 def show_error(exctype, value, tb, gtk_main=False):
+    """
+    Shows window showing detailed information about critical error.
+
+    :param exctype: converted to string and displayed (may contain additional text)
+    :param value: converted to string and displayed
+    :param tb: converted to string and displayed
+    :param gtk_main: True if Gtk.main() should be called.
+        It can be used if program crashes before main loop is started elsewhere.
+    """
     def on_button_clicked(*_):
         window.close()
 
@@ -25,6 +34,12 @@ def show_error(exctype, value, tb, gtk_main=False):
 
 
 def catch_exception(function):
+    """
+    Can be used to catch any exception and display error message. This shouldn't be used.
+
+    :param function: function to be decorated
+    :return: decorated function
+    """
     @functools.wraps(function)
     def inner(*args, **kwargs):
         try:
@@ -36,10 +51,30 @@ def catch_exception(function):
 
 
 def catch_global_exception(exctype, value, tb):
+    """
+    Shows window showing detailed information about critical error. Also puts standard error message into console.
+    It is expected that main Gtk loop is started.
+    This can be used as global exception hook.
+    See `show_error` function for more details.
+
+    :param exctype: exception type
+    :param value: value
+    :param tb: traceback
+    """
     show_error(exctype, value, tb, gtk_main=False)
     sys.__excepthook__(exctype, value, tb)
 
 
 def catch_global_exception_with_gtk_main(exctype, value, tb):
+    """
+    Shows window showing detailed information about critical error. Also puts standard error message into console.
+    It is expected that main Gtk loop is not started.
+    This can be used as global exception hook.
+    See `show_error` function for more details.
+
+    :param exctype: exception type
+    :param value: value
+    :param tb: traceback
+    """
     show_error(exctype, value, tb, gtk_main=True)
     sys.__excepthook__(exctype, value, tb)
